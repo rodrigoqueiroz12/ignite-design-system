712884f371bfef074e7e73fb5318f6e2c48563d6
Rodrigo Rocha de Queiroz <rodrigo@emperium.com.br>
2024-01-19 20:54:42 -0300

    update

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -27,7 +27,7 @@ const config: StorybookConfig = {
   },
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
-      config.base = 'ignite-design-system'
+      config.base = '/ignite-design-system/'
     }
 
     return config
